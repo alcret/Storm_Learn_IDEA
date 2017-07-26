a53d32d3e0cb2bd9da1c37f7692e1ce788b8cc29
alcret <alcret@163.com>
2017-07-26 11:00:23 +0800

    subModel

--- src/main/java/four/JsonFormatter.java
+++ src/main/java/four/JsonFormatter.java
@@ -8,6 +8,9 @@ import java.util.logging.LogRecord;
  * Created by zc on 17-7-25.
  */
 public class JsonFormatter extends Formatter {
+    private static final String QUOTE = "\"";
+    private static final String COLON = ":";
+    private static final String COMMA = ",";
 
     public String format(LogRecord record) {
         return null;
